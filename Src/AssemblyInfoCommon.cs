d879beb5fe29c95491b163eeae74203e0b7a4d39
Kent Boogaart <kent.boogaart@gmail.com>
2014-09-25 23:20:41 +0930

    Version bump.

--- Src/AssemblyInfoCommon.cs
+++ Src/AssemblyInfoCommon.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Resources;
 
 // this is used to version artifacts. AssemblyInformationalVersion should use semantic versioning (http://semver.org/)
-[assembly: AssemblyInformationalVersion("0.0.1-alpha")]
+[assembly: AssemblyInformationalVersion("0.0.2-alpha")]
 [assembly: AssemblyVersion("0.0.1.0")]
 
 [assembly: AssemblyCompany("Kent Boogaart")]
